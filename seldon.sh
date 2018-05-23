57552682fdb771a3022fb85ed8f7c37fc175081a
Enrique <engapa@gmail.com>
2018-05-23 15:04:58 +0200

    Add flag '--wait' to helm command

--- seldon.sh
+++ seldon.sh
@@ -4,8 +4,10 @@ set -e
 
 [ -z "$GITHUB_TOKEN" ] && echo "Env variable GITHUB_TOKEN not defined" && exit 1
 
+cd ${DCOS_HOME}
+
 HELM_URL=https://storage.googleapis.com/kubernetes-helm/helm-$(curl -H "Cache-Control: no-cache" -H "Authorization: token ${GITHUB_TOKEN}" -s https://api.github.com/repos/kubernetes/helm/releases/latest | jq -r ".tag_name")-linux-amd64.tar.gz
-curl -L -H "Authorization: token ${GITHUB_TOKEN}" $HELM_URL -o helm.tar.gz
+curl -L $HELM_URL -o helm.tar.gz
 
 tar -xzvf helm.tar.gz --wildcards --no-anchored 'helm' --strip-components=1
 mv ./helm /usr/local/bin/
@@ -13,14 +15,12 @@ mv ./helm /usr/local/bin/
 kubectl -n kube-system create sa tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 
-
-helm init --service-account tiller
+helm init --wait --service-account tiller
 
 helm install seldon-core-crd --name seldon-core-crd \
      --repo https://storage.googleapis.com/seldon-charts \
      --set usage_metrics.enabled=true \
      --set rbac.enabled=false
-
 
 kubectl create namespace seldon
 
