dfa6bde495719b43b2eb3899749ce2c74d7ce30d
Enrique <engapa@gmail.com>
2018-05-28 16:35:44 +0200

    Seldon installation with helm requirement

--- seldon.sh
+++ seldon.sh
@@ -6,17 +6,6 @@ cd ${DCOS_HOME}
 
 [ -z "$HELM_INSTALLED" ] && echo "Helm and tiller must be installed ..." && ./helm.sh
 
-HELM_URL=https://storage.googleapis.com/kubernetes-helm/helm-$(curl -H "Cache-Control: no-cache" -H "Authorization: token ${GITHUB_TOKEN}" -s https://api.github.com/repos/kubernetes/helm/releases/latest | jq -r ".tag_name")-linux-amd64.tar.gz
-curl -L $HELM_URL -o helm.tar.gz
-
-tar -xzvf helm.tar.gz --wildcards --no-anchored 'helm' --strip-components=1
-mv ./helm /usr/local/bin/
-
-kubectl -n kube-system create sa tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-
-helm init --wait --service-account tiller
-
 helm install seldon-core-crd --name seldon-core-crd \
      --repo https://storage.googleapis.com/seldon-charts \
      --set usage_metrics.enabled=true \
